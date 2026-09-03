a95face348d6473d2537641bb22d372cc5c3ef20
Eugen Scutaru <evgensc5@gmail.com>
2026-09-03 21:32:24 +0300

    Implement multilingual country and city application

--- src/main/java/Country/City/CityRestControler.java
+++ src/main/java/Country/City/CityRestControler.java
@@ -1,41 +1,54 @@
 package Country.City;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CityRestControler {
-    @GetMapping("/")
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String showlanguage() {
-        return "Alege țara:_______/RM <br>" +
-                "Выберите страну:_________/RU <br>" +
-                "Choose a country:________/EN <br>" +
-                "Choisissez un pays :_____/FR <br>" +
-                "Wählen Sie ein Land aus:_/DE ";
+        return "<h1>Alege tara</h1>" +
+                "<a href='/RM'>Republica Moldova</a><br>" +
+                "<a href='/RU'>Россия</a><br>" +
+                "<a href='/EN'>United Kingdom</a><br>" +
+                "<a href='/FR'>France</a><br>" +
+                "<a href='/DE'>Deutschland</a>";
     }
 
-    @GetMapping("/RM")
+    @GetMapping(value = "/RM", produces = MediaType.TEXT_HTML_VALUE)
     public String showrm() {
-        return "orașul: Chișinău";
+        return "<h1>Oras: Chisinau</h1>" +
+                "<img src='/images/Chisinau.jpg' alt='Chisinau' width='400'>" +
+                "<br><br><a href='/'>Inapoi</a>";
     }
 
-    @GetMapping("/RU")
+    @GetMapping(value = "/RU", produces = MediaType.TEXT_HTML_VALUE)
     public String showru() {
-        return "город: Москва";
+        return "<h1>Город: Москва</h1>" +
+                "<img src='/images/Moscova.jpg' alt='Москва' width='400'>" +
+                "<br><br><a href='/'>Назад</a>";
     }
 
-    @GetMapping("/EN")
+    @GetMapping(value = "/EN", produces = MediaType.TEXT_HTML_VALUE)
     public String showen() {
-        return "Cities: London";
+        return "<h1>City: London</h1>" +
+                "<img src='/images/London.jpg' alt='London' width='400'>" +
+                "<br><br><a href='/'>Back</a>";
     }
 
-    @GetMapping("/FR")
+    @GetMapping(value = "/FR", produces = MediaType.TEXT_HTML_VALUE)
     public String showfr() {
-        return "Villes: Paris";
+        return "<h1>Ville : Paris</h1>" +
+                "<img src='/images/Paris.jpg' alt='Paris' width='400'>" +
+                "<br><br><a href='/'>Retour</a>";
     }
 
-    @GetMapping("/DE")
+    @GetMapping(value = "/DE", produces = MediaType.TEXT_HTML_VALUE)
     public String showde() {
-        return "Städte: Berlin";
+        return "<h1>Stadt: Berlin</h1>" +
+                "<img src='/images/Berlin.jpg' alt='Berlin' width='400'>" +
+                "<br><br><a href='/'>Zurück</a>";
     }
 }
