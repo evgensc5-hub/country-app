28611f179c23cc77ff911d2414b10354a33be52f
Eugen Scutaru <evgensc5@gmail.com>
2026-09-03 20:52:16 +0300

    Add new class CityRestControler

--- src/main/java/Country/City/CityRestControler.java
+++ src/main/java/Country/City/CityRestControler.java
@@ -1,1 +1,41 @@
+package Country.City;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CityRestControler {
+    @GetMapping("/")
+    public String showlanguage() {
+        return "Alege țara:_______/RM <br>" +
+                "Выберите страну:_________/RU <br>" +
+                "Choose a country:________/EN <br>" +
+                "Choisissez un pays :_____/FR <br>" +
+                "Wählen Sie ein Land aus:_/DE ";
+    }
+
+    @GetMapping("/RM")
+    public String showrm() {
+        return "orașul: Chișinău";
+    }
+
+    @GetMapping("/RU")
+    public String showru() {
+        return "город: Москва";
+    }
+
+    @GetMapping("/EN")
+    public String showen() {
+        return "Cities: London";
+    }
+
+    @GetMapping("/FR")
+    public String showfr() {
+        return "Villes: Paris";
+    }
+
+    @GetMapping("/DE")
+    public String showde() {
+        return "Städte: Berlin";
+    }
+}
