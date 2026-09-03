f71be0cadcdea7549f7a30b7ec05bd915faed9de
Eugen Scutaru <evgensc5@gmail.com>
2026-09-03 20:27:51 +0300

    Initial commit

---
+++ src/test/java/Country/City/CityApplicationTests.java
@@ -0,0 +1,13 @@
+package Country.City;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
